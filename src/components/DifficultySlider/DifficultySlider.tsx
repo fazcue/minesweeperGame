a4c0933b1c93f6b4d9d41f82b6bb92bdca3c3c9f
Facundo Azcue <fazcue@gmail.com>
2024-01-15 02:19:39 -0300

    Add DifficultySlider component

---
+++ src/components/DifficultySlider/DifficultySlider.tsx
@@ -0,0 +1,26 @@
+import styles from './DifficultySlider.module.css'
+import useStore from '@/store/store'
+
+export default function DifficultySlider() {
+	const { boardSize, setMines, mines } = useStore((state) => state)
+
+	const changeSlider = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setMines({ ...mines, total: +e.target.value })
+	}
+
+	return (
+		<>
+			<h2>difficulty</h2>
+			<input
+				type="range"
+				min="1"
+				max={boardSize.rows * boardSize.columns - 1}
+				value={mines.total}
+				className={styles.slider}
+				step="1"
+				onChange={changeSlider}
+			></input>
+			<small>{mines.total} mines</small>
+		</>
+	)
+}
