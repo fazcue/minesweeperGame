28068af65889ccfb47b455e51ad76cdeea7c4491
Facundo Azcue <fazcue@gmail.com>
2024-01-21 02:28:54 -0300

    Add Timer + replace 'hasWon' with 'winner' + remove use of wonModal

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -3,27 +3,17 @@
 import useStore from '@/store/store'
 import Cell from '../Cell/Cell'
 import Confetti from 'react-confetti'
-import { wonModal } from '@/utils/alerts'
 import styles from './Board.module.css'
 import Helpers from '../Helpers/Helpers'
+import Timer from '../Timer/Timer'
 
 export default function Board(): JSX.Element {
-	const { boardSize, mines, board, resetGame, changeSettings } = useStore(
-		(state) => state
-	)
-
-	const hasWon = () => {
-		const toReveal = boardSize.rows * boardSize.columns - mines.total // 15
-		const totalRevealed = board
-			.flat()
-			.reduce((acc, cell) => acc + (cell.revealed ? 1 : 0), 0)
-
-		return totalRevealed === toReveal
-	}
+	const { mines, board, winner } = useStore()
 
 	return (
 		<>
 			<p>Mines to discover: {mines.total - mines.discovered}</p>
+			<Timer />
 			{board.map((row, i) => (
 				<div key={i} className={styles.row}>
 					{row.map((cell) => (
@@ -32,12 +22,7 @@ export default function Board(): JSX.Element {
 				</div>
 			))}
 			<Helpers />
-			{hasWon() && (
-				<>
-					<Confetti />
-					{wonModal({ resetGame, changeSettings })}
-				</>
-			)}
+			{winner && <Confetti />}
 		</>
 	)
 }
