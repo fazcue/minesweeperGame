409d9fad6a078b28c93cb4d80846d20ce9d8d166
Facundo Azcue <fazcue@gmail.com>
2024-01-21 19:12:26 -0300

    Add Confetti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,22 @@
 import useStore from '@/store/store'
 import Board from '@/components/Board/Board'
 import Options from '@/components/Options/Options'
-import React from 'react'
-
+import Confetti from 'react-confetti'
 export default function Home(): React.JSX.Element {
 	const playing = useStore((state) => state.playing)
+	const winner = useStore((state) => state.winner)
 
 	return (
 		<div className="container" onContextMenu={(e) => e.preventDefault()}>
 			<h1>MineSweeper</h1>
-			{!playing ? <Options /> : <Board />}
+			{!playing ? (
+				<Options />
+			) : (
+				<>
+					<Board />
+					{winner && <Confetti />}
+				</>
+			)}
 		</div>
 	)
 }
