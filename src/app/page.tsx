88d984fb7e9e645b25b54e9637c1a28dd1196b4d
Facundo Azcue <fazcue@gmail.com>
2024-01-24 03:17:28 -0300

    Convert to 'server' component + Add CustomGame && GameList components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,14 @@
-'use client'
+import CustomGame from '@/components/CustomGame/CustomGame'
+import EmptySpace from '@/components/EmptySpace/EmptySpace'
+import GameList from '@/components/GameList/GameList'
 
-import useStore from '@/store/store'
-import Board from '@/components/Board/Board'
-import Options from '@/components/Options/Options'
-import Confetti from 'react-confetti'
 export default function Home(): React.JSX.Element {
-	const playing = useStore((state) => state.playing)
-	const winner = useStore((state) => state.winner)
-
 	return (
-		<div className="container" onContextMenu={(e) => e.preventDefault()}>
+		<div className="container">
 			<h1>MineSweeper</h1>
-			{!playing ? (
-				<Options />
-			) : (
-				<>
-					<Board />
-					{winner && <Confetti />}
-				</>
-			)}
+			<CustomGame />
+			<EmptySpace height={50} />
+			<GameList />
 		</div>
 	)
 }
