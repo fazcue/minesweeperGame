4b3e102ced5e41184e0cbbf2653f6555837bcbcd
Facundo Azcue <fazcue@gmail.com>
2024-01-24 03:15:39 -0300

    Add [slug] route for preset games

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import PresetGame from '@/components/PresetGame/PresetGame'
+import { getGamesSlugs, getGames } from '@/db/client'
+
+interface Props {
+	params: {
+		slug: string
+	}
+}
+
+export async function generateStaticParams() {
+	return getGamesSlugs()
+}
+
+async function getGame(slug: string) {
+	const games = await getGames()
+	return games.find((game) => game.slug === slug)
+}
+
+async function GamePage({ params }: Props) {
+	const game = await getGame(params.slug)
+
+	if (!game) {
+		return <div>Game not found</div>
+	}
+
+	return <PresetGame game={game} />
+}
+
+export default GamePage
