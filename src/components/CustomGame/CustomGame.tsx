8d4261351aede728c706a21874c3db223d27c67d
Facundo Azcue <fazcue@gmail.com>
2024-01-25 04:24:02 -0300

    Add useEffect to set playing to false as default

--- src/components/CustomGame/CustomGame.tsx
+++ src/components/CustomGame/CustomGame.tsx
@@ -4,10 +4,17 @@ import Options from '@/components/Options/Options'
 import Board from '@/components/Board/Board'
 import useStore from '@/store/store'
 import Confetti from 'react-confetti'
+import { useEffect } from 'react'
 
 export default function CustomGame() {
 	const playing = useStore((state) => state.playing)
 	const winner = useStore((state) => state.winner)
+
+	useEffect(() => {
+		useStore.setState({
+			playing: false,
+		})
+	}, [])
 
 	return (
 		<div
