b910166fa7f47091a1739a0771d657b41fbd8404
Facundo Azcue <fazcue@gmail.com>
2024-01-25 01:05:35 -0300

    Update GameList to use GameCard

--- src/components/GameList/GameList.tsx
+++ src/components/GameList/GameList.tsx
@@ -1,7 +1,6 @@
-import { Fragment } from 'react'
 import { getGames } from '@/db/client'
-import Link from 'next/link'
 import EmptySpace from '../EmptySpace/EmptySpace'
+import GameCard from './GameCard'
 
 export default async function GameList() {
 	const games = await getGames()
@@ -12,13 +11,7 @@ export default async function GameList() {
 				<h2>Game List</h2>
 				<EmptySpace />
 				{games.map((game) => (
-					<Fragment key={game.id}>
-						<Link href={`/${game.slug}`}>
-							<h3>{game.name}</h3>
-						</Link>
-						<p>{game.description}</p>
-						<p>{game.mines}</p>
-					</Fragment>
+					<GameCard key={game.id} game={game} />
 				))}
 			</>
 		)
